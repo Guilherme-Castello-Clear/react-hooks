a145e290431c4fac472cc6d8a2a4156a77095d04
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-27 12:56:15 -0300

    Feat: Added useContext

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 import App from './views/App'
 
+import DataContext, {data} from './data/DataContex'
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
